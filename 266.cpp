ecbc7c46a560fbbc384f2b6b6e91a2c5d17cfa27
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-14 01:10:10 +0530

    266. Palindrome Permutation

---
+++ 266.cpp
@@ -0,0 +1,47 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPermutePalindrome(string s) {
+		int memory[260];
+		
+		memset(memory, 0, sizeof(memory));
+
+		int len = s.length();
+
+		for(int i = 0; i < len; i++)
+			memory[s[i]]++;
+
+		int numOfOdds = 0;
+
+		if(len&1)
+		{
+			for(int i = 0; i < 260; i++)
+			{
+				if(memory[i]&1)
+					numOfOdds++;
+
+				if(numOfOdds > 1)
+					return false;
+			}
+
+			return true;
+		}
+		else
+		{
+			for(int i = 0; i < 260; i++)
+				if(memory[i]&1)
+					return false;
+
+			return true;
+		}
+	}
+};
