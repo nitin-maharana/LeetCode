6ef746f1b5201e7ce6280b2864254ab61b5b6864
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-13 23:33:31 +0530

    292. Nim Game

---
+++ 292.cpp
@@ -0,0 +1,14 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4)
+            return true;
+        else
+            return false;
+    }
+};
