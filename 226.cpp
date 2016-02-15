557b44816920d3ef7af0ddcf83a7a3ab38de5e69
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-15 10:23:53 +0530

    226. Invert Binary Tree

---
+++ 226.cpp
@@ -0,0 +1,29 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return root;
+            
+        TreeNode *temp;
+        
+        temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        
+        return root;
+    }
+};
