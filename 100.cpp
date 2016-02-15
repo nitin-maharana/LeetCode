a1580e884d5bad149bf5c75dda59753a9b17f7d1
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-15 11:56:17 +0530

    100. Same Tree

---
+++ 100.cpp
@@ -0,0 +1,26 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
+            return true;
+            
+        if((p && q == NULL) || (p == NULL && q) || (p->val != q->val))
+            return false;
+            
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
