0b67eabc935aa8be0188ee4e5d18cbfe2af2dae6
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-16 11:38:16 +0530

    206. Reverse Linked List (Recursive Version)

---
+++ 206_Recursive.cpp
@@ -0,0 +1,25 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+            
+        ListNode *newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
