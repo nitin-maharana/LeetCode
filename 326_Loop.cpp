bd1c9086e24a1377b8a8798e5ec4909aca20d71e
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-17 22:38:46 +0530

    326. Power of Three (Using Loop)

---
+++ 326_Loop.cpp
@@ -0,0 +1,17 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 0)
+            return false;
+            
+        while(n%3 == 0)
+            n /= 3;
+        
+        return (n==1);
+    }
+};
