55c2ebf1b9d97e26160920edfba2ddcda4e699ae
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-20 12:42:47 +0530

    9. Palindrome Number (One more solution added)

--- 9.cpp
+++ 9.cpp
@@ -5,6 +5,38 @@
 
 class Solution {
 public:
+    bool isPalindrome1(int x) {
+        if(x < 0)
+            return false;
+            
+        int temp = x;
+        int len = 0;
+        
+        while(temp)
+        {
+            len++;
+            temp /= 10;
+        }
+        
+        int left, right;
+        int leftPower = pow(10, len-1);
+        int rightPower = 1;
+        
+        while(leftPower >= rightPower)
+        {
+            left = (x / leftPower) % 10;
+            right = (x / rightPower) % 10;
+            
+            if(left != right)
+                return false;
+                
+            leftPower /= 10;
+            rightPower *= 10;
+        }
+        
+        return true;
+    }
+
     bool isPalindrome(int x) {
         
         if(x < 0)
