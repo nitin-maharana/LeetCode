25d1fe934df9c19f1d767010a2d1e7f8afeb5cdb
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-22 19:44:59 +0530

    190. Reverse Bits (Without swap)

--- 190.cpp
+++ 190.cpp
@@ -5,6 +5,19 @@
 
 class Solution {
 public:
+	uint32_t reverseBits1(uint32_t n) {
+        uint32_t result = 0;
+        
+        for(int i = 0; i < 32; i++)
+        {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+
+        return result;
+    }
+
     uint32_t reverseBits(uint32_t n) {
         uint32_t l, r, temp, shift;
         
