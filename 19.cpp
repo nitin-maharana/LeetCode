306f03b9d8d398e63fa1d85601a082308d3be2e6
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-22 21:25:31 +0530

    19. Remove Nth Node From End of List (1-Pass Algo)

--- 19.cpp
+++ 19.cpp
@@ -24,6 +24,41 @@ class Solution {
             head = head->next;
         }
     }
+
+    int removeNodeFromEnd1(ListNode* head, int n)
+    {
+        if(!head)
+            return 0;
+            
+        int num;
+        
+        num = removeNodeFromEnd1(head->next, n);
+        
+        if(num == n)
+            head->next = head->next->next;
+            
+        return num+1;
+    }
+    
+    void removeNodeFromEnd2(ListNode* head, int n)
+    {
+        ListNode *ptr1, *ptr2;
+        
+        ptr1 = head;
+        
+        for(int i = 0; i < n; i++)
+            ptr1 = ptr1->next;
+            
+        ptr2 = head;
+        
+        while(ptr1->next)
+        {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        
+        ptr2->next = ptr2->next->next;
+    }
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(!head)
@@ -43,5 +78,18 @@ public:
         ptr->next = ptr->next->next;
         
         return head;
+    }
+
+    ListNode* removeNthFromEnd1(ListNode* head, int n) {
+        ListNode newHead(1);
+        newHead.next = head;
+        head = &newHead;
+        
+        if(random() % 2)
+            removeNodeFromEnd1(head, n);
+        else
+            removeNodeFromEnd2(head, n);
+            
+        return head->next;
     }
 };
