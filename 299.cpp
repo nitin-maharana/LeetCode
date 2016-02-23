2d52462c190929d67a33fbbd2e931d495f8077fa
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-23 12:33:29 +0530

    299. Bulls and Cows (Space Efficient - Using one Hash Table)

--- 299.cpp
+++ 299.cpp
@@ -37,4 +37,31 @@ public:
         
         return result;
     }
+
+    string getHint1(string secret, string guess) {
+        len = secret.length();
+        
+        int memory[10] = {0};
+        int bulls = 0;
+        int cows = 0;
+        
+        for(int i = 0; i < len; i++)
+        {
+            if(secret[i] == guess[i])
+                bulls++;
+            else
+                memory[secret[i]-'0']++;
+        }
+        
+        for(int i = 0; i < len; i++)
+        {
+            if(secret[i] != guess[i] && memory[guess[i]-'0'] > 0)
+            {
+                cows++;
+                memory[guess[i]-'0']--;
+            }
+        }
+        
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
 };
