3f88ce01721a1b72ddda9400d954bd9f11dc775f
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-26 11:00:58 +0530

    231. Power of Two

--- 231.cpp
+++ 231.cpp
@@ -5,13 +5,13 @@
  
 class Solution {
 public:
-	bool anotherSolution(int n)
-	{
-		if(n <= 0)
-			return false;
+    bool anotherSolution(int n)
+    {
+        if(n <= 0)
+            return false;
 
-		return ((n & ~-n) == 0);
-	}
+        return ((n & ~-n) == 0);
+    }
 
     bool isPowerOfTwo(int n) {
         if(n <= 0)
