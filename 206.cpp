e40fd2a4ef9565c8e3b05271db5651e6bdcdf164
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-26 10:52:13 +0530

    206. Reverse Linked List

--- 206.cpp
+++ 206.cpp
@@ -11,6 +11,8 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//Iterative Solution.
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -31,3 +33,18 @@ public:
         return prev;
     }
 };
+
+//Recursive Solution.
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+            
+        ListNode *newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
+
