ee4dc45484a2a7a2f20a382987ca4ee4f9788e97
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-27 17:23:47 +0530

    238. Product of Array Except Self

---
+++ 238.cpp
@@ -0,0 +1,31 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result(nums.size(), 0);
+        
+        int leftProd, rightProd;
+        
+        leftProd = 1;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            result[i] = leftProd;
+            leftProd *= nums[i];
+        }
+        
+        rightProd = 1;
+
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            result[i] = result[i] * rightProd;
+            rightProd *= nums[i];
+        }
+        
+        return result;
+    }
+};
