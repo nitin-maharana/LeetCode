b7f1461d452e06ad903ff6234c5974d424434be6
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-28 23:26:10 +0530

    268. Missing Number

---
+++ 268.cpp
@@ -0,0 +1,35 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+//Using XOR.
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int result = 0;
+        
+        for(int i = 1; i <= nums.size(); i++)
+            result ^= (i ^ nums[i-1]);
+
+        return result;
+    }
+};
+
+//Using Summation.
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int len = nums.size();
+        
+        int wholeSum = (len * (len + 1)) / 2;
+
+        int arraySum = 0;
+        
+        for(int i : nums)
+            arraySum += i;
+            
+        return wholeSum - arraySum;
+    }
+};
