88e8a9132a6d14f0db18f917b7189c9db17eaeba
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-02-29 22:57:52 +0530

    94. Binary Tree Inorder Traversal

---
+++ 94.cpp
@@ -0,0 +1,67 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+//Iterative Solution.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        
+        if(!root)
+            return result;
+        
+        stack<TreeNode*> memory;
+        
+        while(!memory.empty() || root)
+        {
+            if(!root)
+            {
+                if(!memory.empty())
+                {
+                    root = memory.top();
+                    memory.pop();
+                    result.push_back(root->val);
+                    root = root->right;
+                }
+            }
+            else
+            {
+                memory.push(root);
+                root = root->left;
+            }
+        }
+        
+        return result;
+    }
+};
+
+//Recursive Solution.
+class Solution {
+    vector<int> result;
+    
+    void inorderTraversalUtil(TreeNode *root)
+    {
+        if(!root)
+            return;
+        
+        inorderTraversalUtil(root->left);
+        result.push_back(root->val);
+        inorderTraversalUtil(root->right);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorderTraversalUtil(root);
+        return result;
+    }
+};
