4ba1520f4a0437cd1aca67b6a38e70982c577eb7
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-03-01 22:43:17 +0530

    35. Search Insert Position

---
+++ 35.cpp
@@ -0,0 +1,32 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l, r, mid;
+        
+        l = 0;
+        r = nums.size()-1;
+        
+        while(l < r)
+        {
+            mid = l + ((r - l) / 2);
+            
+            if(target == nums[mid])
+                return mid;
+
+            if(target > nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        
+        if(target == nums[l])
+            return l;
+    
+        return target > nums[l] ? l+1 : l;
+    }
+};
