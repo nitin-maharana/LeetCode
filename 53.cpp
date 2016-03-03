f56b49fa298e2cbd4301187184150f0baa40f03c
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-03-03 13:15:31 +0530

    53. Maximum Subarray (Divide & Conquer Solution)

--- 53.cpp
+++ 53.cpp
@@ -3,6 +3,7 @@
  * nitin.maharana@gmail.com
  */
 
+//Kadane's Algorithm
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -20,5 +21,49 @@ public:
         }
         
         return maxSumSoFar;
+    }
+};
+
+//Divide and Conquer
+class Solution {
+    int maxSubFromCross(vector<int>& nums, int l, int m, int r)
+    {
+        int left = INT_MIN;
+        int currSum = 0;
+        
+        for(int i = m; i >= l; i--)
+        {
+            currSum += nums[i];
+            
+            if(currSum > left)
+                left = currSum;
+        }
+        
+        int right = INT_MIN;
+        currSum = 0;
+        
+        for(int i = m+1; i <= r; i++)
+        {
+            currSum += nums[i];
+            
+            if(currSum > right)
+                right = currSum;
+        }
+        
+        return (left + right);
+    }
+    
+    int maxSubArray(vector<int>& nums, int l, int r)
+    {
+        if(l == r)
+            return nums[l];
+        
+        int m = l + ((r-l)/2);
+        
+        return max(max(maxSubArray(nums, l, m), maxSubArray(nums, m+1, r)), maxSubFromCross(nums, l, m, r));
+    }
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, nums.size()-1);
     }
 };
