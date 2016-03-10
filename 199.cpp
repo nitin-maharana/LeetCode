b9a6f398c2af8ebc2f7747e1f45bb29f5abb6e1a
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-03-10 21:21:55 +0530

    199. Binary Tree Right Side View - Iterative Solution.

--- 199.cpp
+++ 199.cpp
@@ -13,6 +13,58 @@
  * };
  */
 
+//Iterative Solution - 2-Queue method.
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        
+        if(!root)
+            return result;
+        
+        queue<TreeNode*> memory1, memory2;
+        TreeNode *last;
+        
+        memory1.push(root);
+        
+        while(!memory1.empty() || !memory2.empty())
+        {
+            if(!memory1.empty())
+            {
+                while(!memory1.empty())
+                {
+                    last = memory1.front();
+                    memory1.pop();
+                    
+                    if(last->left)
+                        memory2.push(last->left);
+                    
+                    if(last->right)
+                        memory2.push(last->right);
+                }
+            }
+            else
+            {
+                while(!memory2.empty())
+                {
+                    last = memory2.front();
+                    memory2.pop();
+                    
+                    if(last->left)
+                        memory1.push(last->left);
+                    
+                    if(last->right)
+                        memory1.push(last->right);
+                }
+            }
+            
+            result.push_back(last->val);
+        }
+        
+        return result;
+    }
+};
+
 //Recursive Solution.
 class Solution {
     vector<int> result;
