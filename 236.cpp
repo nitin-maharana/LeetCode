521aa5dab1d357f846cfe0df31224b26651ac44b
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-04-05 01:38:34 +0530

    236. Lowest Common Ancestor of a Binary Tree - One more solution added [No Assumption]

--- 236.cpp
+++ 236.cpp
@@ -12,6 +12,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//Solution [Assuming both nodes are present]
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -27,5 +28,65 @@ public:
             return root;
         
         return (left ? left : right);
+    }
+};
+
+//Solution [With no such assumption] [If any of the node is not present, returns NULL]
+class Solution {
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q, bool& p1, bool& q1)
+    {
+        if(root == NULL)
+            return NULL;
+
+        TreeNode *left, *right;
+        
+        if(root == p)
+        {
+            p1 = true;
+            return root;
+        }
+        
+        if(root == q)
+        {
+            q1 = true;
+            return root;
+        }
+        
+        left = lowestCommonAncestor(root->left, p, q, p1, q1);
+        right = lowestCommonAncestor(root->right, p, q, p1, q1);
+        
+        if(left && right)
+            return root;
+
+        return (left ? left : right);
+    }
+    
+    TreeNode* findAnother(TreeNode *root, TreeNode *n)
+    {
+        if(root == NULL)
+            return NULL;
+        
+        if(root == n)
+            return root;
+        
+        TreeNode *left, *right;
+        left = findAnother(root->left, n);
+        right = findAnother(root->right, n);
+        
+        return (left ? left : right);
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+            return NULL;
+        
+        bool p1 = false, q1 = false;
+        
+        TreeNode *lca = lowestCommonAncestor(root, p, q, p1, q1);
+        
+        if((p1 && q1) || (p1 && findAnother(lca, q)) || (q1 && findAnother(lca, p)))
+            return lca;
+        else
+            return NULL;
     }
 };
