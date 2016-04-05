162a81d28b72c0a36e733d6da1df47a6f7a93221
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-04-05 15:52:41 +0530

    235. Lowest Common Ancestor of a Binary Search Tree - One more recursive solution added.
    It doens't assume anything. If any node is not present, it returns NULL.
    The nodes need not to be distinct.

--- 235.cpp
+++ 235.cpp
@@ -50,3 +50,44 @@ public:
         return root;
     }
 };
+
+//Recursive Solution. It doesn't assume anything. If any node is not present, returns NULL.
+//As well as no need to be distinct nodes.
+class Solution {
+    TreeNode* lcaUtil(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(root == nullptr || root == p || root == q)
+            return root;
+        
+        if(root->val < p->val && root->val < q->val)
+            return lcaUtil(root->right, p, q);
+        
+        if(root->val > p->val && root->val > q->val)
+            return lcaUtil(root->left, p, q);
+        
+        return root;
+    }
+    
+    TreeNode* findNode(TreeNode* root, TreeNode* n)
+    {
+        if(root == nullptr || root == n)
+            return root;
+        
+        if(root->val < n->val)
+            return findNode(root->right, n);
+        else
+            return findNode(root->left, n);
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q)
+            return root;
+        
+        TreeNode *lca = lcaUtil(root, p, q);
+        
+        if(findNode(lca, p) && findNode(lca, q))
+            return lca;
+        
+        return NULL;
+    }
+};
