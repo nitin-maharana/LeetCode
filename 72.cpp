a3b1ee683fee073a7a628e72a252e8a38d9cccd7
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-04-14 21:18:03 +0530

    72. Edit Distance - Iterative Solution

--- 72.cpp
+++ 72.cpp
@@ -35,3 +35,35 @@ public:
         return minDistance(word1, word2, memory, m, n);
     }
 };
+
+//Iterative Solution.
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m, n;
+        
+        m = word1.length();
+        n = word2.length();
+        
+        vector<vector<int>> memory(m+1, vector<int>(n+1, 0));
+        
+        for(int i = 0; i <= m; i++)
+            memory[i][0] = i;
+        
+        for(int j = 0; j <= n; j++)
+            memory[0][j] = j;
+        
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                    memory[i][j] = memory[i-1][j-1];
+                else
+                    memory[i][j] = 1 + min(memory[i-1][j-1], min(memory[i-1][j], memory[i][j-1]));
+            }
+        }
+        
+        return memory[m][n];
+    }
+};
