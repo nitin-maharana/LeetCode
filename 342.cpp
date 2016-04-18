c7ac16964ebf016a5d1ecf20d839f60705c9e538
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-04-19 00:43:05 +0530

    342. Power of Four - One more solution added.

--- 342.cpp
+++ 342.cpp
@@ -24,3 +24,18 @@ public:
         return (num > 0 && (num & (num-1)) == 0 && (num & 0xAAAAAAAA) == 0);
     }
 };
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num <= 0)
+            return false;
+        
+        if(num == 1)
+            return true;
+
+        double temp = log(num) / log(4);
+        
+        return (temp == (int)temp);
+    }
+};
